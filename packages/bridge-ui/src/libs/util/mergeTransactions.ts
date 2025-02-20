b2d02ceeb0a9578ce130f2bee6d315310ff2c314
Jithin KS <jithinks112@gmail.com>
2025-02-20 22:13:45 +0530

    Update mergeTransactions.ts

--- packages/bridge-ui/src/libs/util/mergeTransactions.ts
+++ packages/bridge-ui/src/libs/util/mergeTransactions.ts
@@ -11,7 +11,7 @@ export const mergeAndCaptureOutdatedTransactions = (
 ): MergeResult => {
   const relayerTxMap: Map<string, BridgeTransaction> = new Map();
   relayerTx.forEach((tx) => relayerTxMap.set(tx.srcTxHash, tx));
-  const addedRelayerTxHashMap = new Map<string, boolean>();
+  const addedRelayerTxSet = new Set<string>();
 
   const outdatedLocalTransactions: BridgeTransaction[] = [];
   const mergedTransactions: BridgeTransaction[] = [];
@@ -25,11 +25,11 @@ export const mergeAndCaptureOutdatedTransactions = (
   }
 
   for (const tx of relayerTx) {
-    if (addedRelayerTxHashMap.has(tx.srcTxHash)) {
+    if (addedRelayerTxSet.has(tx.srcTxHash)) {
       continue;
     }
     mergedTransactions.push(tx);
-    addedRelayerTxHashMap.set(tx.srcTxHash, true);
+    addedRelayerTxSet.add(tx.srcTxHash);
   }
 
   return { mergedTransactions, outdatedLocalTransactions };
