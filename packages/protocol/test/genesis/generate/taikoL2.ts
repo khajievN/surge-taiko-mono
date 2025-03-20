7f892f3ec0a1e13b1152c1babcdc19d710a56523
Anshu Jalan <anshujalan206@gmail.com>
2025-03-21 01:01:28 +0530

    feat: remove pausing from genesis (#104)

--- packages/protocol/test/genesis/generate/taikoL2.ts
+++ packages/protocol/test/genesis/generate/taikoL2.ts
@@ -328,7 +328,7 @@ async function generateContractConfigs(
             variables: {
                 // EssentialContract
                 __reentry: 1, // _FALSE
-                __paused: 2, // _TRUE
+                __paused: 1, // _FALSE
                 // EssentialContract => UUPSUpgradeable => Initializable
                 _initialized: 1,
                 _initializing: false,
